6ef13793921e9d594a48dd5fc2d99b92f480a0e1
Tahereh Sharifian <tahereh.sharifian97@gmail.com>
2023-01-25 16:28:10 +0330

    Blog upload

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import BlogList from "../BlogList/BlogList";
+import useFetch from "../../CustomHooks/useFetch";
+
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
+ 
+export default Home;
